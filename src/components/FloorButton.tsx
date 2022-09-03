d66dcb7263c28f6dec2163bc749bcb8a30fc04df
Dz0526 <this@dz99.me>
2022-09-04 00:51:05 +0900

    Style: change MapPin & add hover

--- src/components/FloorButton.tsx
+++ src/components/FloorButton.tsx
@@ -13,9 +13,9 @@ export const FloorButton = ({ floor, setFloor }: Props) => {
     <p className='absolute bottom-0 right-0'>
       {floorNumbers.map(num => (
         <button
-          className={`p-3 text-5xl ${floor !== num && 'hover:text-blue-300'} ${
-            floor === num && 'text-slate-200'
-          }`}
+          className={`p-3 text-5xl ${
+            floor !== num && 'hover:text-blue-300 hover:text-7xl'
+          } ${floor === num && 'text-slate-200'}`}
           disabled={floor === num}
           key={num}
           value={num}
--- src/components/MapPin.tsx
+++ src/components/MapPin.tsx
@@ -1,30 +1,29 @@
-import { Star, Wedge } from 'react-konva';
+import { Label, Tag, Text } from 'react-konva';
 
 type Props = {
   x: number;
   y: number;
+  name: string;
 };
 
-export const MapPin = ({ x, y }: Props) => {
+export const MapPin = ({ x, y, name }: Props) => {
   return (
     <>
-      <Star
-        numPoints={5}
-        stroke='skyblue'
-        strokeWidth={3}
-        innerRadius={5}
-        outerRadius={10}
-        x={x}
-        y={y}
-      />
-      <Wedge
-        angle={20}
-        radius={15}
-        fill='skyblue'
-        x={x}
-        y={y + 28}
-        rotationDeg={260}
-      />
+      <Label x={x} y={y} opacity={0.75}>
+        <Tag
+          fill='black'
+          pointerDirection='down'
+          pointerWidth={10}
+          pointerHeight={10}
+          lineJoin='round'
+          shadowColor='black'
+          shadowBlur={10}
+          shadowOffsetX={10}
+          shadowOffsetY={10}
+          shadowOpacity={0.5}
+        />
+        <Text text={name} fill='white' fontSize={18} />
+      </Label>
     </>
   );
 };
